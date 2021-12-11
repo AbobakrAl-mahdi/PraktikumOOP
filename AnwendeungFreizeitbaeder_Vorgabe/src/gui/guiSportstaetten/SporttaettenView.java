fdb8a902de67800e45f288e90c3e469e019a3b3e
Abobakr Al-mahdi <74077606+Abobakral-mahdi@users.noreply.github.com>
2021-12-11 17:48:02 +0100

    Sportstaetten Fehler Korriegert

--- AnwendeungFreizeitbaeder_Vorgabe/src/gui/guiSportstaetten/SporttaettenView.java
+++ AnwendeungFreizeitbaeder_Vorgabe/src/gui/guiSportstaetten/SporttaettenView.java
@@ -5,6 +5,7 @@ import gui.guiFreizeitbaeder.FreizeitBaederControler;
 import javafx.event.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -63,15 +64,15 @@ public class SporttaettenView {
  			new EventHandler<ActionEvent>() {
 	    		@Override
 	        	public void handle(ActionEvent e) {
-	            	zeigeFreizeitbadAn();
+	            	zeigeFreizeitbaederAn();
 	        	} 
    	    });
     }
    
     private void zeigeFreizeitbaederAn(){
-    		if(freizeitbaederModel.getFreizeitbad() != null){
+    		if(freizeitBaederModel.getFreizeitbad() != null){
     			txtAnzeigeFreizeitbaeder.setText(
-    				freizeitbaederModel.getFreizeitbad()
+    				freizeitBaederModel.getFreizeitbad()
  				.gibFreizeitbadZurueck(' '));
     		}
     		else{
@@ -81,8 +82,8 @@ public class SporttaettenView {
     }	
    
     private void zeigeInformationsfensterAn(String meldung){
-    	  	new MeldungsfensterAnzeiger(AlterType.INFORMATION,
-               	"Information", nmeldung).zeigeMeldungsfensterAn();
+    	  	new MeldungsfensterAnzeiger(AlertType.INFORMATION,
+               	"Information", meldung).zeigeMeldungsfensterAn();
     }	
     
 }
